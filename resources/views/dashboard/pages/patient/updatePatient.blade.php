40f3d3c925100396e2daaf9941ab35a66d565485
ahmedsalah95 <ahmedsalah9570@gmail.com>
2018-09-02 06:10:07 +0200

    adding question of patient

--- resources/views/dashboard/pages/patient/updatePatient.blade.php
+++ resources/views/dashboard/pages/patient/updatePatient.blade.php
@@ -233,6 +233,80 @@
                         </div>
                     </div>
 
+                    <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="dont_eat"> اطعمة تسبب الحساسية للمريض <span class="required">*</span>
+                        </label>
+
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+
+                            <input type="text" value="<?php $patient->sensitivity ? $patient->sensitivity : "" ?>" name="sensitivity" id="sensitivity" data-role="tagsinput" class="form-control col-md-7 col-xs-12">
+                        </div>
+                    </div>
+
+
+                    <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="blood"> ادوية مميعه للدم <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <input type="text" value="" name="blood" id="blood" data-role="tagsinput" class="form-control col-md-7 col-xs-12"
+
+                            >
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="sleeping_hours">عدد ساعات النوم
+                            <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <input type="text" name="sleeping_hours"
+
+                                   id="sleeping_hours" class=" form-control col-md-7 col-xs-12" required="required" >
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="water">كمية الماء المستهلك يوميا
+                            <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <input type="text" name="water"
+                                   value="{{old('water')}}"
+                                   id="water" class=" form-control col-md-7 col-xs-12" required="required" >
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="diet">هل قمت بعمل روجيم من قبل؟
+                            <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <input type="text" name="diet"
+                                   value="{{old('diet')}}"
+                                   id="diet" class=" form-control col-md-7 col-xs-12" required="required" >
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="diet_name">ما اسم النظام الذي اتبعته؟
+                            <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <input type="text" name="diet_name"
+                                   value="{{old('diet_name')}}"
+                                   id="diet_name" class=" form-control col-md-7 col-xs-12" required="required" >
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="diet_result">كم خسرت من الوزن؟
+                            <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <input type="text" name="diet_result"
+                                   value="{{old('diet_result')}}"
+                                   id="diet_result" class=" form-control col-md-7 col-xs-12" required="required" >
+                        </div>
+                    </div>
 
 
                     <div class="form-group">
