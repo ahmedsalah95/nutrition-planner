a4c635672b0dbf8659c3c7df1a87c30d4fd31307
ahmedsalah95 <ahmedsalah9570@gmail.com>
2018-09-04 16:53:03 +0200

    final version before beta 2

--- database/migrations/2018_08_24_130619_create_notes_table.php
+++ database/migrations/2018_08_24_130619_create_notes_table.php
@@ -16,7 +16,7 @@ class CreateNotesTable extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('patient_id');
-            $table->longText('note');
+            $table->longText('note')->nullable();
             $table->timestamps();
         });
     }
